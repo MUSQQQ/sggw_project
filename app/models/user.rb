3a627ec85311363afe35ce3d8282418715c67529
Jan Walczak <super.walczak@gmail.com>
2021-03-06 01:03:07 +0100

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    
+   validates :username , presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 20}
+   
+   #constans in ruby are declared using all CAPS
+   REGEX_PATTERN = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #a regex form for checking correctness of an email
+
+   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 128}, format: {with: REGEX_PATTERN}
+
 end
